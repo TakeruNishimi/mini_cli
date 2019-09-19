80fe9f2a50cab44af6e2df33c50599c7cf5bdfa9
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-19 14:41:03 +0900

    割り勘の計算式にdivmod()関数を使用

--- split_app.py
+++ split_app.py
@@ -2,10 +2,11 @@ def main():
     amount = int(input("金額を入力してください[円] : "))
     number_of_people = int(input("人数を入力してください[人] : "))
 
-
-    pay = amount//number_of_people
-    remainder = amount-(pay*number_of_people)
+    # pay = amount//number_of_people
+    # remainder = amount-(pay*number_of_people)
+    pay, remainder = divmod(amount, number_of_people)
     print(f'{amount}円{number_of_people}人　→　1人あたり{pay}円です。端数は{remainder}円です。')
+
 
 if __name__ == '__main__':
     main()
