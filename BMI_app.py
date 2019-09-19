6de6f7408ed96c07b1ee9557ccdf5143b1bad72c
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-19 14:32:14 +0900

    BMI計算サプリを実装した。

---
+++ BMI_app.py
@@ -0,0 +1,9 @@
+def main():
+    height = float(input("身長を入力してください[cm] : "))
+    weight = float(input("体重を入力してください[kg] : "))
+    bmi = round((weight / (height / 100) ** 2), 2)
+    print("BMIは ",bmi)
+
+
+if __name__ == '__main__':
+    main()
